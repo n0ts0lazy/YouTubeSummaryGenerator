903fa38fc136c382439dff6b8e2ac953d68dc67d
Arnab <arnabsarkar2000@gmail.com>
2023-09-20 00:11:11 +0530

    Revert "Commit Temp"
    This reverts commit 661367045321184b6b72a507ca17056091617f32.

--- dirCheck.py
+++ dirCheck.py
@@ -1,23 +1,18 @@
 # Import os module
 import os
 
-# Function to create folders if they don't exist
+# Function to create 'video cache' and 'result' folders if they don't exist
 def createFolders():
     try:
-        # Check if 'Video cache' folder exists, and create it if not
+        # Check if 'video cache' folder exists, and create it if not
         video_cache_folder = 'Video cache'
         if not os.path.exists(video_cache_folder):
             os.makedirs(video_cache_folder)
 
-        # Check if 'Result' folder exists, and create it if not
+        # Check if 'result' folder exists, and create it if not
         result_folder = 'Result'
         if not os.path.exists(result_folder):
             os.makedirs(result_folder)
-
-        # Check if 'Audio Cache' folder exists, and create it if not
-        audio_cache_folder = 'Audio Cache'
-        if not os.path.exists(audio_cache_folder):
-            os.makedirs(audio_cache_folder)
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
--- main.py
+++ main.py
@@ -4,17 +4,16 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 from dirCheck import createFolders
 
-# Function to download the video, audio, and transcript
-def download_video_audio_and_transcript(youtube_url):
-    # Create 'Video cache', 'Audio Cache', and 'Result' folders if they don't exist
+# Function to download the video and transcript
+def download_video_and_transcript(youtube_url):
+    # Create 'Video cache' and 'Result' folders if they don't exist
     createFolders()
 
     # Create a YouTube object
     yt = YouTube(youtube_url)
 
-    # Get the highest quality streams for video and audio
+    # Get the highest quality stream of the video
     video_stream = yt.streams.get_highest_resolution()
-    audio_stream = yt.streams.filter(only_audio=True).first()
 
     # Get the video title and author for reference
     video_title = yt.title
@@ -23,27 +22,23 @@ def download_video_audio_and_transcript(youtube_url):
     # Get the video ID
     video_id = yt.video_id
 
-    # Set the file path for the downloaded video in 'Video Cache' folder
+    # Set the file path for the downloaded video in 'video cache' folder
     print('Downloading video', video_title)
-    video_cache_folder = 'Video Cache'
+    video_cache_folder = 'Video cache'
+    video_file_path = os.path.join(video_cache_folder, f"{video_title}.mp4")
 
-    # Set the file path for the downloaded audio in 'Audio Cache' folder
-    print('Downloading audio', video_title)
-    audio_cache_folder = 'Audio Cache'
-
-    # Download the video and audio to their respective folders
+    # Download the video to the 'video cache' folder
     video_stream.download(output_path=video_cache_folder)
-    audio_stream.download(output_path=audio_cache_folder)
 
     # Get the transcript using the YouTubeTranscriptApi
     transcript = YouTubeTranscriptApi.get_transcript(video_id)
 
-    # Set the file path for the transcript file in 'Result' folder
+    # Set the file path for the transcript file in 'result' folder
     print('Downloading transcript', video_title)
     result_folder = 'Result'
     transcript_file = os.path.join(result_folder, f"{video_title}_transcript.txt")
 
-    # Create a text file for the transcript in the 'Result' folder
+    # Create a text file for the transcript in the 'result' folder
     with open(transcript_file, "w", encoding="utf-8") as file:
         file.write(f"Video Title: {video_title}\n")
         file.write(f"Video Author: {video_author}\n\n")
@@ -56,5 +51,5 @@ if __name__ == "__main__":
     # Prompt the user for the YouTube URL
     youtube_url = input("Enter the YouTube URL: ")
 
-    # Call the download_video_audio_and_transcript function
-    download_video_audio_and_transcript(youtube_url)
+    # Call the download_video_and_transcript function
+    download_video_and_transcript(youtube_url)
