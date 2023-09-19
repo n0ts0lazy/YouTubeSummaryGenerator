9fbbb7594799cc311a1ece87a2bd49bbe42cdb15
Arnab <arnabsarkar2000@gmail.com>
2023-09-20 00:48:50 +0530

    Commit #2
    Repo name changed to reflect its current purpose and added a new functionality
    > Added Audio Stream download option

--- dirCheck.py
+++ dirCheck.py
@@ -1,13 +1,18 @@
 # Import os module
 import os
 
-# Function to create 'video cache' and 'result' folders if they don't exist
+# Function to create 'video cache', 'audio cache', and 'result' folders if they don't exist
 def createFolders():
     try:
         # Check if 'video cache' folder exists, and create it if not
-        video_cache_folder = 'Video cache'
+        video_cache_folder = 'Video Cache'
         if not os.path.exists(video_cache_folder):
             os.makedirs(video_cache_folder)
+
+        # Check if 'audio cache' folder exists, and create it if not
+        audio_cache_folder = 'Audio Cache'
+        if not os.path.exists(audio_cache_folder):
+            os.makedirs(audio_cache_folder)
 
         # Check if 'result' folder exists, and create it if not
         result_folder = 'Result'
--- main.py
+++ main.py
@@ -4,16 +4,17 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 from dirCheck import createFolders
 
-# Function to download the video and transcript
-def download_video_and_transcript(youtube_url):
-    # Create 'Video cache' and 'Result' folders if they don't exist
+# Function to download the video, audio, and transcript
+def download_video_audio_and_transcript(youtube_url):
+    # Create 'Video Cache', 'Audio Cache', and 'Result' folders if they don't exist
     createFolders()
 
     # Create a YouTube object
     yt = YouTube(youtube_url)
 
-    # Get the highest quality stream of the video
+    # Get the highest quality stream of the video and audio
     video_stream = yt.streams.get_highest_resolution()
+    audio_stream = yt.streams.filter(only_audio=True).first()
 
     # Get the video title and author for reference
     video_title = yt.title
@@ -22,23 +23,29 @@ def download_video_and_transcript(youtube_url):
     # Get the video ID
     video_id = yt.video_id
 
-    # Set the file path for the downloaded video in 'video cache' folder
-    print('Downloading video', video_title)
-    video_cache_folder = 'Video cache'
+    # Set the file path for the downloaded video and audio in 'Video Cache' and 'Audio Cache' folders
+    video_cache_folder = 'Video Cache'
+    audio_cache_folder = 'Audio Cache'
     video_file_path = os.path.join(video_cache_folder, f"{video_title}.mp4")
+    audio_file_path = os.path.join(audio_cache_folder, f"{video_title}.mp3")
 
-    # Download the video to the 'video cache' folder
+    # Download the video to the 'Video Cache' folder
+    print('Downloading video', video_title)
     video_stream.download(output_path=video_cache_folder)
 
+    # Download the audio to the 'Audio Cache' folder
+    print('Downloading audio', video_title)
+    audio_stream.download(output_path=audio_cache_folder)
+
     # Get the transcript using the YouTubeTranscriptApi
+    print('Creating transcript', video_title)
     transcript = YouTubeTranscriptApi.get_transcript(video_id)
 
-    # Set the file path for the transcript file in 'result' folder
-    print('Downloading transcript', video_title)
+    # Set the file path for the transcript file in 'Result' folder
     result_folder = 'Result'
     transcript_file = os.path.join(result_folder, f"{video_title}_transcript.txt")
 
-    # Create a text file for the transcript in the 'result' folder
+    # Create a text file for the transcript in the 'Result' folder
     with open(transcript_file, "w", encoding="utf-8") as file:
         file.write(f"Video Title: {video_title}\n")
         file.write(f"Video Author: {video_author}\n\n")
@@ -46,10 +53,9 @@ def download_video_and_transcript(youtube_url):
             text = entry['text']
             file.write(text + "\n")
 
-
 if __name__ == "__main__":
     # Prompt the user for the YouTube URL
     youtube_url = input("Enter the YouTube URL: ")
 
-    # Call the download_video_and_transcript function
-    download_video_and_transcript(youtube_url)
+    # Call the download_video_audio_and_transcript function
+    download_video_audio_and_transcript(youtube_url)
